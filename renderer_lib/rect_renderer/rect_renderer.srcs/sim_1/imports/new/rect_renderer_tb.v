d5fca49ffb1236a04084063f2f0db36eff4d5b48
Db2515 <daniel@boulby.plus.com>
2019-04-09 17:04:14 +0100

    Add tests for shape position and dimension change

--- renderer_lib/rect_renderer/rect_renderer.srcs/sim_1/imports/new/rect_renderer_tb.v
+++ renderer_lib/rect_renderer/rect_renderer.srcs/sim_1/imports/new/rect_renderer_tb.v
@@ -43,7 +43,11 @@ module rect_renderer_tb();
         .y_out(y_out),
         .color_out(color_out));
         
-    initial begin
+        reg [31:0] expected;
+        integer i;
+        integer j;
+        
+    initial begin 
         // Set shape to cover entire screen as black rect
         program = 1;
         x = 0;
@@ -56,11 +60,12 @@ module rect_renderer_tb();
         program = 0;
         //Set background color to BLUE this should not been seen as shape is full screen
         color_in = 'hFF0000FF;
-        for(integer i = 0; i < 1080; i = i + 135) begin
+        for(i = 0; i < 1080; i = i + 135) begin
             x = i;
-            for(integer j = 0; j < 2160; j = j + 135) begin
+            for(j = 0; j < 2160; j = j + 135) begin
                 y = j;
-                if (color_out != 'hFF000000) $display("Initial Setup: %d %d Actual color_out %h, Expected color_out %h", i, j, color_out, 'hFF000000); 
+                expected = 'hFF000000;
+                if (color_out != expected) $display("Initial Setup: %d %d Actual color_out %h, Expected color_out %h", i, j, color_out, expected); 
                 #20;
             end
         end
@@ -76,14 +81,65 @@ module rect_renderer_tb();
         //Set background color to BLUE this should not been seen as shape is full screen
         // Instead we should see RED
         color_in = 'hFF0000FF;
-        for(integer i = 0; i < 1080; i = i + 135) begin
+        for(i = 0; i < 1080; i = i + 135) begin
             x = i;
-            for(integer j = 0; j < 2160; j = j + 135) begin
+            for(j = 0; j < 2160; j = j + 135) begin
                 y = j;
-                if (color_out != 'hFFFF0000) $display("Red Color: %d %d Actual color_out %h, Expected color_out %h", i, j, color_out, 'hFFFF0000); 
+                expected = 'hFFFF0000;
+                if (color_out != expected) $display("Red Color: %d %d Actual color_out %h, Expected color_out %h", i, j, color_out, expected); 
                 #20;
             end
         end
+        
+        // Test shape dimension change
+        // Half the width, shape_color still RED
+        program = 1;
+        x = 0;
+        y = 0;
+        color_in = 'hFFFF0000;
+        shape_width = 540;
+        #40;
+        program = 0;
+        //Set background color to BLUE this should not been seen as shape is full screen
+        // Instead we should see RED
+        color_in = 'hFF0000FF;
+        for(i = 0; i < 1080; i = i + 135) begin
+            x = i;
+            for(j = 0; j < 2160; j = j + 135) begin
+                y = j;
+                #20;
+                expected = i < shape_width ? 'hFFFF0000 : 'hFF0000FF;
+                if (color_out != expected) begin
+                    $display("Half Width: %d %d Actual color_out %h, Expected color_out %h", i, j, color_out, 'hFFFF0000); 
+                end
+                #20;
+            end
+        end
+        
+        // Test shape position change
+        // Move the rect to second half of screen
+        program = 1;
+        x = 540;
+        y = 0;
+        color_in = 'hFFFF0000;
+        #40;
+        program = 0;
+        //Set background color to BLUE this should not been seen as shape is full screen
+        // Instead we should see RED
+        color_in = 'hFF0000FF;
+        for(i = 0; i < 1080; i = i + 135) begin
+            x = i;
+            for(j = 0; j < 2160; j = j + 135) begin
+                y = j;
+                #20;
+                expected = i < shape_width ? 'hFF0000FF : 'hFFFF0000;
+                if (color_out != expected) begin
+                    $display("Half Width: %d %d Actual color_out %h, Expected color_out %h", i, j, color_out, 'hFFFF0000); 
+                end
+                #20;
+            end
+        end
+        
         $finish;
     end
     
